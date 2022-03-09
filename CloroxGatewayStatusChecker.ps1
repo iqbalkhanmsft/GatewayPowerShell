ec2d652d27ce82aff98334d550fb3ea4bec5006c
Iqbal Khan <iqbalkhan@microsoft.com>
2022-03-08 20:11:19 -0500

    This is my first commit!

---
+++ CloroxGatewayStatusChecker.ps1
@@ -0,0 +1,44 @@
+#For Clorox POC.
+#Can use app ID and secret.
+Login-DataGatewayServiceAccount
+
+$Primary = Get-DataGatewayCluster
+#Get-DataGatewayClusterStatus -GatewayClusterId 1da235ff-5d0f-41a4-945c-062e07ca307e
+
+$Members =
+ForEach ($cluster in $Primary)
+    {
+
+        Write-Host $Name
+        ForEach ($member in (Get-DataGatewayClusterStatus -GatewayClusterId $cluster.Id))
+
+        {
+
+            [pscustomobject]@{
+
+                PrimaryId = $cluster.Id
+                PrimaryName = $cluster.Name
+                PrimaryDescription = $cluster.Description
+                PrimaryType = $cluster.Type
+                MemberStatus = $member.ClusterStatus
+                MemberVersion = $member.GatewayVersion
+                MemberUpgradeState = $member.GatewayUpgradeState
+                #Need to figure out how to convert list to string in order to get error message.
+                #MemberErrorMessages = $member.MemberGatewayErrorMessages
+
+
+
+            }
+
+
+        }
+    }
+
+$Dir = "C:\Temp\GatewayStatusExport.csv"
+
+#$Members | Where-Object {$Members.MemberStatus -like 'None'}
+
+#Change to Live if wanting to show gateways that are active.
+$Members | Where-Object {$Members.MemberStatus -like 'None'} | Export-Csv $Dir -NoTypeInformation -Encoding utf8
+
+#Disconnect-DataGatewayServiceAccount
